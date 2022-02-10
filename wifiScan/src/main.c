4fc531c834bf561046e407df29d7573b18e55ee9
egemeric <egemeric99@gmail.com>
2022-02-10 17:41:51 +0300

    update wifi scan

--- wifiScan/src/main.c
+++ wifiScan/src/main.c
@@ -1,55 +1,75 @@
 #include "wifi_scan.h"
-#include <stdio.h>  //printf
+#include <stdio.h>	//printf
 #include <unistd.h> //sleep
 
-//convert bssid to printable hardware mac address
 const char *bssid_to_string(const uint8_t bssid[BSSID_LENGTH], char bssid_string[BSSID_STRING_LENGTH])
 {
 	snprintf(bssid_string, BSSID_STRING_LENGTH, "%02x:%02x:%02x:%02x:%02x:%02x",
-         bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5]);
+			 bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5]);
 	return bssid_string;
 }
+
+const int BSS_INFOS = 50;
 
 void Usage(char **argv);
 
 int main(int argc, char **argv)
 {
-	struct wifi_scan *wifi=NULL;    //this stores all the library information
-	struct station_info station;    //this is where we are going to keep information about AP (Access Point) we are connected to
-	char mac[BSSID_STRING_LENGTH];  //a placeholder where we convert BSSID to printable hardware mac address
-	int status;
+	struct wifi_scan *wifi = NULL;
+	struct wifi_scan *wifi5 = NULL;
+	struct bss_info bss[BSS_INFOS];
+	char mac[BSSID_STRING_LENGTH];
+	int status, i;
+	int status5, i5;
 
-	if(argc != 2)
+	if (argc < 2 && argc >3)
 	{
 		Usage(argv);
 		return 0;
 	}
 
-	printf("This is just example, this is library - not utility\n");
-	printf("### Close the program with ctrl+c when you're done ###\n\n");
-	
-	// initialize the library with network interface argv[1] (e.g. wlan0)
-	wifi=wifi_scan_init(argv[1]);
-
-	while(1)
+	wifi = wifi_scan_init(argv[1]);
+	if (argc == 3)
 	{
-		//get information from just the station we are associated with
-		//this is quick, you can call it at much faster frequency (e.g. 50 ms)
-		status=wifi_scan_station(wifi, &station);
-		
-		if(status==0)
-			printf("No associated station\n");
-		else if(status==-1)
-			perror("Unable to get station information\n");
+		wifi5 = wifi_scan_init(argv[2]);
+		status5 = wifi_scan_all(wifi5, bss, BSS_INFOS);
+		if (status5 < 0)
+			perror("Unable to get scan data (second interface)");
 		else
-			printf("%s %s signal %d dBm avg %d dBm rx %u tx %u \n",
-				bssid_to_string(station.bssid, mac), station.ssid,
-				station.signal_dbm, station.signal_avg_dbm,
-				station.rx_packets, station.tx_packets);
-		sleep(1);
+		{
+			for (i5 = 0; i < status5 && i5 < BSS_INFOS; ++i)
+				printf("INT2) %s %s signal %d dBm on frequency %u MHz seen %d ms ago status %s\n",
+					   bssid_to_string(bss[i].bssid, mac),
+					   bss[i].ssid,
+					   bss[i].signal_mbm / 100,
+					   bss[i].frequency,
+					   bss[i].seen_ms_ago,
+					   (bss[i].status == BSS_ASSOCIATED ? "associated" : ""));
+
+			printf("\n");
+		}
+		wifi_scan_close(wifi5);
+	}
+	while (1)
+	{
+		status = wifi_scan_all(wifi, bss, BSS_INFOS);
+		if (status < 0)
+			perror("Unable to get scan data");
+		else
+			for (i = 0; i < status && i < BSS_INFOS; ++i)
+				printf("%s %s signal %d dBm on frequency %u MHz seen %d ms ago status %s\n",
+					   bssid_to_string(bss[i].bssid, mac),
+					   bss[i].ssid,
+					   bss[i].signal_mbm / 100,
+					   bss[i].frequency,
+					   bss[i].seen_ms_ago,
+					   (bss[i].status == BSS_ASSOCIATED ? "associated" : ""));
+
+		printf("\n");
+
+		sleep(2);
 	}
 
-	//free the library resources
 	wifi_scan_close(wifi);
 
 	return 0;
@@ -61,5 +81,4 @@ void Usage(char **argv)
 	printf("%s wireless_interface\n\n", argv[0]);
 	printf("examples:\n");
 	printf("%s wlan0\n", argv[0]);
-	
 }
